6d6bea370dd5ec494734928fa76570b6acc7f5e2
xin.liu <1665677303@qq.com>
2026-03-17 17:29:37 +0800

    Form组件1提交

---
+++ Form/src/Form/Form.tsx
@@ -0,0 +1,65 @@
+import { useState, useRef, type CSSProperties, type ReactElement, type FormEvent } from "react";
+import FormContext from "./FormContext";
+import classNames from "classnames";
+
+interface FormProps {
+    className?: string,
+    style?: CSSProperties,
+    initialValues?: Record<string, any>,
+    onFinish?: (values: Record<string, any>) => void,
+    onFinishFailed?: (errors: Record<string, any>) => void,
+    children?: ReactElement
+}
+
+const Form = (props: FormProps) => {
+    const { className, style, initialValues, onFinish, onFinishFailed, children } = props;
+    const [values, setValues] = useState<Record<string, any>>(initialValues || {});
+    const errors = useRef<Record<string, any>>({});
+    const validatorMap = useRef(new Map<string, Function>());
+
+    // 单个值变化时
+    const onValueChange = (key: string, value: any) => {
+        values[key] = value;
+    }
+
+    // 提交时，走一遍所有校验器内的校验函数
+    // 函数返回的错误保存在errors对象内部
+    // 根据errors的非null，执行成功失败函数
+    const handleSubmit = (e: FormEvent) => {
+        e.preventDefault();
+        for (let [key, cbfunction] of validatorMap.current) {
+            if (typeof cbfunction === 'function') {
+                errors.current[key] = cbfunction()
+            }
+        }
+
+        // 去除null值
+        const errorsList = Object.values(errors.current).filter(Boolean);
+
+        // 根据错误长度，执行函数
+        if (errorsList.length) {
+            onFinishFailed?.(errors.current)
+        } else {
+            onFinish?.(values)
+        }
+    };
+
+    const validateRegister = (name: string, cbFunction: Function) => {
+        validatorMap.current.set(name, cbFunction)
+    };
+
+    const cl = classNames('ant-form', className)
+
+    return (
+        <FormContext.Provider
+            value={{
+                values: values,
+                setValues: (values) => setValues(values),
+                onValueChange,
+                validateRegister
+            }}>
+                <form className={cl} style={style} onSubmit={handleSubmit}>{children}</form>
+        </FormContext.Provider>
+    )
+};
+export default Form;
---
+++ Form/src/Form/FormContext.tsx
@@ -0,0 +1,14 @@
+import { createContext } from "react";
+
+export interface FromContextProps {
+    // 汇总表单键值对
+    values?: Record<string, any>,
+    // 更新--汇总表单键值对
+    setValues?: (values:Record<string, any>)=> void,
+    // 单个表单键值对修改时，更新
+    onValueChange?: (key: string, value: any)=> void,
+    // 校验注册器
+    validateRegister?: (name: string, cb: Function)=> void
+};
+
+export default createContext<FromContextProps>({})
