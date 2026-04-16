0bf85b158aa049d1feae42df4cce525a4fc1e124
xin.liu <1665677303@qq.com>
2026-04-16 17:56:17 +0800

    feat：playground思路

--- playground-test/src/App.tsx
+++ playground-test/src/App.tsx
@@ -1,26 +1,33 @@
 import { transform } from '@babel/standalone';
+import type { PluginObj} from '@babel/core';
 import './App.css';
 import { useRef } from 'react';
 
 function App() {
   const textareaRef = useRef<HTMLTextAreaElement>(null);
 
-  const code = `import { useEffect, useState } from "react";
+  const code1 = `
+    fucntion add( a, b ) {
+      return (a + b)
+    };
 
-  function App() {
-    const [num, setNum] = useState(() => {
-      const num1 = 1 + 2;
-      const num2 = 2 + 3;
-      return num1 + num2
-    });
-  
-    return (
-      <div onClick={() => setNum((prevNum) => prevNum + 1)}>{num}</div>
-    );
-  }
-  
-  export default App;
-  `
+     export { add }
+  `;
+
+  const code2 = `
+    import { add } from './add.ts';
+
+    console.log('add值',add(2, 3));
+  `;
+  const url = URL.createObjectURL(new Blob([code1], {type: 'application/javascript'}));
+
+  const transformImportSourcePlugin: PluginObj = {
+    visitor: {
+      ImportDeclaration(path) {
+        path.node.source.value = url;
+      }
+    }
+  };
 
   function onClick() {
     if (!textareaRef.current) {
@@ -29,15 +36,17 @@ function App() {
 
     const res = transform(textareaRef.current.value, {
       presets: ['react', 'typescript'],
-      filename: 'xin.tsx' 
+      filename: 'xin.tsx' ,
+      plugins: [transformImportSourcePlugin]
     });
 
-    console.log(res.code);
+    console.log('res.code',res.code);
   }
+
 
   return (
     <>
-      <textarea ref={textareaRef} style={{height:'500px', width: '300px' }}defaultValue={code}></textarea>
+      <textarea ref={textareaRef} style={{height:'500px', width: '300px' }}defaultValue={code2}></textarea>
       <button onClick={onClick}>编译</button>
     </>
   )
