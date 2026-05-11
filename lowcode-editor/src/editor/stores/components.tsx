5b85c6d18e6a985583de30bf0ed055d6a25c576d
xin.liu <1665677303@qq.com>
2026-05-11 17:59:02 +0800

    feat

--- lowcode-editor/src/editor/stores/components.tsx
+++ lowcode-editor/src/editor/stores/components.tsx
@@ -1,3 +1,5 @@
+import { create } from 'zustand';
+
 interface ComponentType {
     id: number,
     name: string,
@@ -10,15 +12,23 @@ interface State {
     components: ComponentType[]
 };
 
-function getComponentsById(id: number | null, components: ComponentType[] ): ComponentType | null {
-    if(!id) return null;
+const useComponentsStores = create((set, get) => {
+    componets: [{
+        id: 1,
+        name: 'page',
+        props: {},
+        desc: 
+    }]
+})
 
-    for(const component of components) {
-        if( component.id === id ) return component;
-        if( component.children && component.children.length > 0 ){
-            const result = getComponentsById( id, component.children);
+function getComponentsById(id: number | null, components: ComponentType[]): ComponentType | null {
+    if (!id) return null;
+
+    for (const component of components) {
+        if (component.id === id) return component;
+        if (component.children && component.children.length > 0) {
+            const result = getComponentsById(id, component.children);
             return result;
         }
     }
-
 }
